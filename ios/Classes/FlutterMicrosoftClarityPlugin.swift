0720c32ae17ef995e9665c44458e42729c7bd8c7
Cesar Mesquita <cesar_mesquita@hotmail.com>
2024-10-16 12:41:59 -0300

    iOS

--- ios/Classes/FlutterMicrosoftClarityPlugin.swift
+++ ios/Classes/FlutterMicrosoftClarityPlugin.swift
@@ -1,20 +1,22 @@
 import Flutter
 import UIKit
-import clarity // Supondo que haja um SDK de Clarity para iOS
+import Clarity 
 
-public class SwiftFlutterMicrosoftClarityPlugin: NSObject, FlutterPlugin {
+public class FlutterMicrosoftClarityPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_microsoft_clarity", binaryMessenger: registrar.messenger())
-        let instance = SwiftFlutterMicrosoftClarityPlugin()
+        let instance = FlutterMicrosoftClarityPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        if call.method == "initializeClarity" {
+        if call.method == "init" {
             if let args = call.arguments as? [String: Any],
-               let projectId = args["projectId"] as? String {
+               let projectId = args["projectID"] as? String {
                 // Inicialize o Microsoft Clarity aqui
-                Clarity.start(withProjectId: projectId)
+                let clarityConfig = ClarityConfig(projectId: projectId)
+                ClaritySDK.initialize(config: clarityConfig)
+              
                 result(true)
             } else {
                 result(FlutterError(code: "INVALID_ARGUMENT", message: "Project ID is required", details: nil))
--- ios/flutter_microsoft_clarity.podspec
+++ ios/flutter_microsoft_clarity.podspec
@@ -15,8 +15,10 @@ A new Flutter plugin project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '12.0'
+  s.platform = :ios, '13.0'
 
+  # Adicionar a dependência do pod Clarity
+  s.dependency 'Clarity'
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
