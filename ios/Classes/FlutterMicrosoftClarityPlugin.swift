3e6fd29108feb26a4769ea598b6dfd33aa6b062b
enesuzun2002 <emurat.uzun@gmail.com>
2025-06-09 11:07:01 +0300

    ios: remove deprecated enableSwiftUI_Experimental flag

--- ios/Classes/FlutterMicrosoftClarityPlugin.swift
+++ ios/Classes/FlutterMicrosoftClarityPlugin.swift
@@ -1,10 +1,11 @@
+import Clarity
 import Flutter
 import UIKit
-import Clarity
 
 public class FlutterMicrosoftClarityPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "flutter_microsoft_clarity", binaryMessenger: registrar.messenger())
+        let channel = FlutterMethodChannel(
+            name: "flutter_microsoft_clarity", binaryMessenger: registrar.messenger())
         let instance = FlutterMicrosoftClarityPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
@@ -12,24 +13,27 @@ public class FlutterMicrosoftClarityPlugin: NSObject, FlutterPlugin {
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if call.method == "init" {
             if let args = call.arguments as? [String: Any],
-                let projectId = args["projectID"] as? String {
-                 // Obtenha o userId se estiver presente
+                let projectId = args["projectID"] as? String
+            {
+                // Obtenha o userId se estiver presente
                 let userId = args["userID"] as? String
                 // Inicialize o Microsoft Clarity aqui
-                 let clarityConfig: ClarityConfig
-            if let userId = userId {
-                clarityConfig = ClarityConfig(projectId: projectId, userId: userId, enableSwiftUI_Experimental: true)
-            } else {
-                clarityConfig = ClarityConfig(projectId: projectId, enableSwiftUI_Experimental: true)
-            }
-            ClaritySDK.initialize(config: clarityConfig)
+                let clarityConfig: ClarityConfig
+                if let userId = userId {
+                    clarityConfig = ClarityConfig(projectId: projectId, userId: userId)
+                } else {
+                    clarityConfig = ClarityConfig(projectId: projectId)
+                }
+                ClaritySDK.initialize(config: clarityConfig)
                 ClaritySDK.initialize(config: clarityConfig)
 
                 result(true)
             } else {
-                result(FlutterError(code: "INVALID_ARGUMENT", message: "Project ID is required", details: nil))
+                result(
+                    FlutterError(
+                        code: "INVALID_ARGUMENT", message: "Project ID is required", details: nil))
             }
-       } else if call.method == "getPlatformVersion" {
+        } else if call.method == "getPlatformVersion" {
             result("iOS " + UIDevice.current.systemVersion)
         } else {
             result(FlutterMethodNotImplemented)
